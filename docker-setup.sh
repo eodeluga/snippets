faea0f56fccf9a9cf7a97c7a7bb5b632170b6dbd
Eugene Odeluga <eodeluga@users.noreply.github.com>
2022-03-29 11:46:07 +0100

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -30,3 +30,6 @@ sudo curl -L \
   "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
   -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Add logged in user to docker group
+sudo usermod -a -G docker $(echo $USERNAME)
