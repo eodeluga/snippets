9223ebcc4b38f1bb2c2e5a288adf1859bfd2fea2
Eugene Odeluga <eodeluga@users.noreply.github.com>
2022-03-29 11:29:28 +0100

    Create docker-setup.sh

---
+++ docker-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Install docker engine and docker compose
+
+# Remove existing docker
+sudo snap remove docker
+
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS
+sudo apt-get update -y
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add Docker’s official GPG key:
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+# Add docker repository. Have to use eoan release on Ubuntu 20 which currently lacks a release file
+sudo add-apt-repository -y \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   eoan stable"
+   
+# Install Docker engine
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install Docker compose
+sudo curl -L \
+  "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
+  -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
