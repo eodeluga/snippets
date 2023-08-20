93e6bb2e24b9e32a4787c4859b1ad127f868981e
Eugene Odeluga <eodeluga@users.noreply.github.com>
2023-08-20 03:35:19 +0100

    Rootless Docker now as default install

--- linux-dev-install.sh
+++ linux-dev-install.sh
@@ -3,21 +3,28 @@
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-# Install Docker
-sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg -y
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update -y
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-  sudo usermod -aG docker $USER
-  sudo systemctl enable docker
-  sudo systemctl start docker
+# Install Docker (rootless)
+sudo apt-get install uidmap -y
+curl -fsSL https://get.docker.com/rootless | sh
+sudo curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
+systemctl --user enable docker.service
+systemctl --user start docker.service
+
+# Install Docker (uncomment to install in root mode)
+# sudo apt-get update -y
+# sudo apt-get install ca-certificates curl gnupg -y
+# sudo install -m 0755 -d /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# echo \
+#  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+#  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#  sudo apt-get update -y
+#  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+#  sudo usermod -aG docker $USER
+#  sudo systemctl enable docker
+#  sudo systemctl start docker
 
 # Install Brave
 sudo apt install curl -y
