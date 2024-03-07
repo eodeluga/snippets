2a1d724c8bcfb9cd977058b480d4c9ab1bea7313
Eugene Odeluga <eodeluga@users.noreply.github.com>
2024-03-07 16:03:26 +0000

    Update linux-dev-install.sh

--- linux-dev-install.sh
+++ linux-dev-install.sh
@@ -27,12 +27,12 @@ systemctl --user start docker.service
 #  sudo systemctl start docker
 
 # Install Brave
-# sudo apt install curl -y
-# sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-# echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-# sudo apt update -y
-# sudo apt upgrade -y
-# sudo apt install brave-browser -y
+sudo apt install curl -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install brave-browser -y
 
 # Add paths to .bashrc
 echo '' | tee -a ~/.bashrc
